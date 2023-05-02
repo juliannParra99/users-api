cc22932b553ae2ee6d69c42ae30acb07cd45bc7c
julianParrax <julianfranco.parra@gmail.com>
2023-05-02 02:20:11 -0300

    Added: 8.se agrega configuracion para conexion a DB para crear tabla y columnas

--- config/db.py
+++ config/db.py
@@ -3,8 +3,7 @@
 from sqlalchemy import create_engine, MetaData
 
 #se crea conexion a la db: create_engine, toma comoa rgumentos mi usuario y la db
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+engine = create_engine("mysql+pymysql://root:r@localhost:3306/login")
 
-conn = engine.connect()
 
 meta_data = MetaData()
--- model/users.py
+++ model/users.py
@@ -7,7 +7,7 @@ users = Table('users', meta_data,
               Column('id', Integer,primary_key=True),
               Column('name',String(255),nullable= False),
               Column('username',String(255),nullable= False),
-              Column('user_passw',String(255),nullable= False),)
+              Column('user_passw',String(255),nullable= False))
 
 #esto crea la tabla con los datos cargados en meta_data
 meta_data.create_all(engine)
